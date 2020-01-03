8826151b02e2e55de7c0c4fd8e2a3d2b920eaf71
ajapon88 <ajapon88@gmail.com>
2020-01-03 23:22:39 +0900

    initial commit

---
+++ lib/pronto/pylint.rb
@@ -0,0 +1,8 @@
+require "pronto/pylint/version"
+
+module Pronto
+  module Pylint
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/pronto/pylint_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Pronto::Pylint do
+  it "has a version number" do
+    expect(Pronto::Pylint::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
