da96613928770e58d6ddf96bc9afd05f01a48dd5
ajapon88 <ajapon88@gmail.com>
2020-01-04 03:06:24 +0900

    fix rspec

--- spec/pronto/pylint_spec.rb
+++ spec/pronto/pylint_spec.rb
@@ -1,7 +1,57 @@
 # frozen_string_literal: true
 
-RSpec.describe Pronto::Pylint do
-  it "has a version number" do
-    expect(Pronto::PylintVersion::VERSION).not_to be nil
+require 'spec_helper'
+
+module Pronto
+  RSpec.describe Pylint do
+    let(:pylint) { Pylint.new(patches) }
+    let(:patches) { nil }
+
+    describe '#run' do
+      subject { pylint.run }
+
+      context 'patches are nil' do
+        it { should == [] }
+      end
+
+      context 'no patches' do
+        let(:patches) { [] }
+        it { should == [] }
+      end
+
+      context 'patches with an offense' do
+        include_context 'test repo'
+
+        let(:patches) { repo.show_commit('f44a11a') }
+
+        its(:count) { should == 1 }
+
+        it 'includes the offense message' do
+          expect(subject.first.msg).to include("[C0325] Unnecessary parens after 'print' keyword")
+        end
+      end
+
+      context 'patches with multiple offense' do
+        include_context 'test repo'
+
+        let(:patches) { repo.show_commit('7b7f452') }
+
+        its(:count) { should == 6 }
+
+        it 'returns messages' do
+          expect(subject.map(&:msg))
+            .to match(
+              [
+                a_string_matching("[C0325] Unnecessary parens after 'print' keyword"),
+                a_string_matching("[C0111] Missing module docstring"),
+                a_string_matching("[E0401] Unable to import 'wsgi'"),
+                a_string_matching("[C0103] Constant name \"server\" doesn't conform to UPPER_CASE naming style"),
+                a_string_matching("[W0611] Unused import BaseHTTPServer"),
+                a_string_matching("W0611] Unused import CGIHTTPServer")
+              ]
+            )
+        end
+      end
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
+require 'rspec'
+require 'rspec/its'
+
 require "bundler/setup"
 require "pronto/pylint"
+
+RSpec.shared_context 'test repo' do
+  let(:git) { 'spec/fixtures/test.git/git' }
+  let(:dot_git) { 'spec/fixtures/test.git/.git' }
+
+  before { FileUtils.mv(git, dot_git) }
+  let(:repo) { Pronto::Git::Repository.new('spec/fixtures/test.git') }
+  after { FileUtils.mv(dot_git, git) }
+end
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
