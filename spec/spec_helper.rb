ad05954f05faaf331383d1b1bda11fd873708cf3
ajapon88 <ajapon88@gmail.com>
2020-01-04 13:15:20 +0900

    order rspec configure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,17 +6,6 @@ require 'rspec/its'
 require "bundler/setup"
 require "pronto/pylint"
 
-RSpec.shared_context 'test repo' do
-  let(:git) { 'spec/fixtures/test.git/git' }
-  let(:dot_git) { 'spec/fixtures/test.git/.git' }
-  let(:pylintrc) { 'spec/fixtures/test.git/_pylintrc' }
-  let(:dot_pylintrc) { 'spec/fixtures/test.git/.pylintrc' }
-
-  before { FileUtils.mv(git, dot_git) }
-  let(:repo) { Pronto::Git::Repository.new('spec/fixtures/test.git') }
-  after { FileUtils.mv(dot_git, git) }
-end
-
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
@@ -27,4 +16,15 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+RSpec.shared_context 'test repo' do
+  let(:git) { 'spec/fixtures/test.git/git' }
+  let(:dot_git) { 'spec/fixtures/test.git/.git' }
+  let(:pylintrc) { 'spec/fixtures/test.git/_pylintrc' }
+  let(:dot_pylintrc) { 'spec/fixtures/test.git/.pylintrc' }
+
+  before { FileUtils.mv(git, dot_git) }
+  let(:repo) { Pronto::Git::Repository.new('spec/fixtures/test.git') }
+  after { FileUtils.mv(dot_git, git) }
 end
